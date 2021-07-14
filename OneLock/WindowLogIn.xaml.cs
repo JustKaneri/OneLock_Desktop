51c009a3da0edba9616c5fdf021d42b484214338
Ilya <ilya@yandex.ru>
2021-07-14 20:44:46 +0300

    Add project files.

---
+++ OneLock/WindowLogIn.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OneLock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            BtnShow.Click += BtnShow_Click;
+            BtnLogIn.Click += BtnLogIn_Click;
+            BtnReg.Click += BtnReg_Click;
+
+            SetStyle();
+        }
+
+        private void SetStyle()
+        {
+            string them = "";
+
+            if (File.Exists("LightThem.cnf"))
+            {
+                them = "LightThem";
+            }
+                
+
+            if (File.Exists("DarkThem.cnf"))
+            {
+                them = "DarkThem";
+            }
+
+            if (File.Exists("PurpureThem.cnf"))
+            {
+                them = "PurpureThem";
+            }
+
+
+
+            var uri = new Uri("Styles\\"+them + ".xaml", UriKind.Relative);
+
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+
+        }
+
+        private void BtnReg_Click(object sender, RoutedEventArgs e)
+        {
+            WindowRegestry regestry = new WindowRegestry();
+            this.Hide();
+            regestry.ShowDialog();
+            this.Show();
+            
+        }
+
+        private void BtnLogIn_Click(object sender, RoutedEventArgs e)
+        {
+            WindowMain main = new WindowMain();
+            main.Show();
+            this.Close();
+        }
+
+        private void BtnShow_Click(object sender, RoutedEventArgs e)
+        {
+            if(TbxPass.PasswordChar == '\0')
+            {
+                TbxPass.PasswordChar = '●';
+                TbxOpenPass.Visibility = Visibility.Hidden;
+                TbxPass.Visibility = Visibility.Visible;
+                TbxPass.Password = TbxOpenPass.Text;
+            }
+            else
+            {
+                TbxPass.PasswordChar = '\0';
+                TbxOpenPass.Visibility = Visibility.Visible;
+                TbxPass.Visibility = Visibility.Hidden;
+                TbxOpenPass.Text = TbxPass.Password;
+            }
+            
+        }
+    }
+}
---
+++ OneLock/WindowMain.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace OneLock
+{
+    /// <summary>
+    /// Interaction logic for WindowMain.xaml
+    /// </summary>
+    public partial class WindowMain : Window
+    {
+        public WindowMain()
+        {
+            InitializeComponent();
+            StpPass.Visibility = Visibility.Visible;
+
+            CbxThems.Items.Add("Светлая");
+            CbxThems.Items.Add("Темная");
+            CbxThems.Items.Add("Фиолетовая-зеленая");
+
+            for (int i = 4; i <= 16; i+=4)
+            {
+                CmxLeng.Items.Add(i);
+            }
+
+            CmxLeng.SelectedIndex = 0;
+            CmbPass.SelectedIndex = 0;
+
+            SetStyle();
+        }
+
+        private void SetStyle()
+        {
+            string them = "";
+
+            if (File.Exists("LightThem.cnf"))
+            {
+                them = "LightThem";
+                CbxThems.SelectedIndex = 0;
+            }
+
+
+            if (File.Exists("DarkThem.cnf"))
+            {
+                them = "DarkThem";
+                CbxThems.SelectedIndex = 1;
+            }
+
+            if (File.Exists("PurpureThem.cnf"))
+            {
+                them = "PurpureThem";
+                CbxThems.SelectedIndex = 2;
+            }
+
+
+            var uri = new Uri("Styles\\" + them + ".xaml", UriKind.Relative);
+
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+
+        }
+
+        private void BtnPassw_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ((Button)sender).Opacity = 0.5;
+        }
+
+        private void BtnPassw_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ((Button)sender).Opacity = 1;
+        }
+
+        private void BtnPassw_Click(object sender, RoutedEventArgs e)
+        {
+            StpPass.Visibility = Visibility.Visible;
+            StpPropert.Visibility = Visibility.Hidden;
+            StpCreate.Visibility = Visibility.Hidden;
+        }
+
+        private void BtnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            StpPass.Visibility = Visibility.Hidden;
+            StpPropert.Visibility = Visibility.Hidden;
+            StpCreate.Visibility = Visibility.Visible;
+        }
+
+        private void BtnProperties_Click(object sender, RoutedEventArgs e)
+        {
+            StpPass.Visibility = Visibility.Hidden;
+            StpPropert.Visibility = Visibility.Visible;
+            StpCreate.Visibility = Visibility.Hidden;
+        }
+
+        private void BtnAddPass_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ((Button)sender).Opacity = 0.5;
+        }
+
+        private void BtnAddPass_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ((Button)sender).Opacity = 1;
+        }
+
+        private void BtnGetPass_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditPass_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEditPass_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAddPass_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnGenPass_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CbxThems_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string currentThem = CbxThems.SelectedItem.ToString().ToLower();
+
+            currentThem = currentThem == "светлая" ? "LightThem" : currentThem;
+            currentThem = currentThem == "темная" ? "DarkThem" : currentThem;
+            currentThem = currentThem == "фиолетовая-зеленая" ? "PurpureThem" : currentThem;
+
+            if (File.Exists("LightThem.cnf"))
+                File.Delete("LightThem.cnf");
+
+            if (File.Exists("DarkThem.cnf"))
+                File.Delete("DarkThem.cnf");
+
+            if (File.Exists("PurpureThem.cnf"))
+                File.Delete("PurpureThem.cnf");
+
+            var uri = new Uri("Styles\\" + currentThem+ ".xaml", UriKind.Relative);
+
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+
+
+            File.Create(currentThem + ".cnf");
+        }
+    }
+}
